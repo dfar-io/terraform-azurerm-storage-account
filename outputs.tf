5736e67793efd79a6e8d7a07c7527916993a7559
Dave Farinelli <d@dfar.io>
2021-01-11 14:34:10 -0500

    Added primary key output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "primary_connection_string" {
   description = "Primary Storage Account connection string."
   value       = azurerm_storage_account.sa.primary_connection_string
 }
+
+output "primary_access_key" {
+  description = "Primary Storage Account key."
+  value       = azurerm_storage_account.sa.primary_access_key
+}
